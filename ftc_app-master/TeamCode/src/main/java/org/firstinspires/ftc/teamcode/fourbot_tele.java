d666dd364378e759334a54b77986d7af02115563
arnavkomaragiri <arnav.komaragiri@gmail.com>
2018-09-25 21:53:04 -0400

    Teleop Update
    Memez

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fourbot_tele.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.movement.Mecanum_Drive;
+
+/**
+ * Created by arnav on 10/22/2017.
+ */
+//@Disabled
+@TeleOp(name="four_bot", group="Teleop")
+public class fourbot_tele extends OpMode{
+    DcMotor motors[] = new DcMotor[4];
+    DcMotor arm;
+    DcMotor sweeper;
+    Mecanum_Drive drive;
+    public void init(){
+        motors[0] = hardwareMap.dcMotor.get("up_left");
+        motors[1] = hardwareMap.dcMotor.get("up_right");
+        motors[2] = hardwareMap.dcMotor.get("back_left");
+        motors[3] = hardwareMap.dcMotor.get("back_right");
+
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        sweeper = hardwareMap.get(DcMotor.class, "sweeper");
+
+        drive = new Mecanum_Drive(motors);
+    }
+    public void loop(){
+        double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - (Math.PI / 4);
+        double rightX = gamepad1.right_stick_x;
+
+        drive.drive(r, robotAngle, rightX);
+
+        arm.setPower(gamepad2.right_stick_y);
+        sweeper.setPower(gamepad2.right_trigger);
+
+        telemetry.addData("Angle:", ((180 * robotAngle) / Math.PI));
+    }
+
+
+}
+
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement/Mecanum_Drive.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.movement;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.control.Pid;
+
+public class Mecanum_Drive {
+    private final double ticksPerRevolution = 1000;  // Get for your motor and gearing.
+    private double prevTime;  // The last time loop() was called.
+    private int prevLeftEncoderPosition;   // Encoder tick at last call to loop().
+    private int prevRightEncoderPosition;  // Encoder tick at last call to loop().
+
+    private final double drivePidKp = 1;     // Tuning variable for PID.
+    private final double drivePidTi = 1.0;   // Eliminate integral error in 1 sec.
+    private final double drivePidTd = 0.1;   // Account for error in 0.1 sec.
+    // Protect against integral windup by limiting integral term.
+    private final double drivePidIntMax = 105;  // Limit to max speed.
+    private final double driveOutMax = 1.0;  // Motor output limited to 100%.
+
+    DcMotor motors[];
+
+    private Pid leftDrive = null;
+    private Pid rightDrive = null;
+
+    public Mecanum_Drive(DcMotor names[]){
+        motors = names.clone();
+        motors[0].setDirection(DcMotor.Direction.REVERSE);
+        motors[2].setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void setMotors(DcMotor names[]){
+        motors = names.clone();
+    }
+
+    public DcMotor[] getMotors(){
+        return motors.clone();
+    }
+
+    public String toString(){
+        String output = "";
+        output += Double.toString(motors[0].getPower());
+        output += " " + Double.toString(motors[1].getPower());
+        output += " " + Double.toString(motors[2].getPower());
+        output += " " + Double.toString(motors[3].getPower());
+
+        return output;
+    }
+
+    public void drive(double r, double angle, double rightX){
+        double robotAngle = angle;
+
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
+
+        motors[0].setPower(v1);
+        motors[1].setPower(v2);
+        motors[2].setPower(v3);
+        motors[3].setPower(v4);
+    }
+}
